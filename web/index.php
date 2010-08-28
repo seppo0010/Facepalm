36402cf7566face2164d9b2c1962584f96298ca8
Sebastian Waisbrot <seppo0010@gmail.com>
2010-08-28 18:27:21 -0300

    Fixed encoding issues

--- web/index.php
+++ web/index.php
@@ -39,17 +39,23 @@ $uid = 0;
 if ($facebook->getSession()) {
 	$uid = $facebook->getUser();
 	$can_associate = !Facepalm::facebookUserHasFacepalm($uid);
-	if (!$can_associate) $fb_button = '<a href="remove_facebook.php">' . htmlentities($i18n['facebook_disconnect']) . '</a>';
+	if (!$can_associate) $fb_button = '<a href="remove_facebook.php">' . htmlentities($i18n['facebook_disconnect'], ENT_QUOTES, 'UTF-8') . '</a>';
 	else $fb_button = '';
 } else {
-	$fb_button = '<a href="' . $facebook->getLoginUrl() . '">' . htmlentities($i18n['facebook_disconnect']) . '</a>';
+	$fb_button = '<a href="' . $facebook->getLoginUrl() . '">' . htmlentities($i18n['facebook_disconnect'], ENT_QUOTES, 'UTF-8') . '</a>';
 	$can_associate = FALSE;
 }
 
 ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Facepalm!</title>
+</head>
+<body>
 <script type="text/javascript">
 function confirm_user(username, id) {
-	var text = prompt('<?php echo htmlentities($i18n['reason_to_facepalm_nickname'], ENT_QUOTES); ?>' + username + '?');
+	var text = prompt('<?php echo htmlentities($i18n['reason_to_facepalm_nickname'], ENT_QUOTES, 'UTF-8'); ?>' + username + '?');
 	if (text != null) {
 		location.href = 'index.php?touch=' + id + '&reason=' + encodeURI(text);
 	}
@@ -65,12 +71,12 @@ tr.self td { font-size: 20px; }
 <a href="index.php?lang=es">Spanish</a>
 <?php } ?>
 <br />
-<?php echo nl2br(htmlentities($i18n['description'])); ?>
+<?php echo nl2br(htmlentities($i18n['description'], ENT_QUOTES, 'UTF-8')); ?>
 <?php if (isset($_COOKIE['id-user'])) { ?>
 <?php list($id, $user) = explode('-', $_COOKIE['id-user']); ?>
 <br /><br />
-<?php echo htmlentities($i18n['welcome'] . ' ' . $user); ?><br />
-<a href="javascript:void(null)" onclick="confirm_user('<?php echo htmlentities($user, ENT_QUOTES) . '\',' . $id; ?>); return false" style="text-align:center;border:2px solid #999; line-height:24px;text-decoration:none;display:block;background:#000; color:#fff; width:100px; height: 24px;">Facepalm</a><br />
+<?php echo htmlentities($i18n['welcome'] . ' ' . $user, ENT_QUOTES, 'UTF-8'); ?><br />
+<a href="javascript:void(null)" onclick="confirm_user('<?php echo htmlentities($user, ENT_QUOTES, 'UTF-8') . '\',' . $id; ?>); return false" style="text-align:center;border:2px solid #999; line-height:24px;text-decoration:none;display:block;background:#000; color:#fff; width:100px; height: 24px;">Facepalm</a><br />
 <?php } ?>
 <br /><br />
 <?php
@@ -79,12 +85,12 @@ foreach ($users as $user)
 {
         echo '<tr' . ($user->id != $id ? '' : ' class="self"') . '>';
 		if ($can_associate && $user->facebook_id == '') echo '<td><a href="https://graph.facebook.com/oauth/authorize?client_id=146881561998534&redirect_uri=' . $config['base_url'] . 'associate_facebook.php?id=' . $user->id . '&scope=publish_stream,offline_access">Asociar a Facebook</a></td>';
-		else if ($uid > 0 && $uid == $user->facebook_id) echo '<td>' . htmlentities($i18n['you']) . '</td>';
+		else if ($uid > 0 && $uid == $user->facebook_id) echo '<td>' . htmlentities($i18n['you'], ENT_QUOTES, 'UTF-8') . '</td>';
 		else echo '<td></td>';
-		echo '<td>' , htmlentities($user->nombre, ENT_QUOTES) , '</td>
+		echo '<td>' , htmlentities($user->nombre, ENT_QUOTES, 'UTF-8') , '</td>
 		<td'. ($user->fecha + 60 * 60 * 24 < time() ? ' style="color:#f00"' : '') . '>' .date('Y-m-d H:i:s', $user->fecha) .' </td>
-		<td><a href="javascript:confirm_user(\'' . htmlentities($user->nombre, ENT_QUOTES) . '\',' . $user->id . '); return false">Facepalm!</a></td>
-		<td><a href="index.php?borrar=' . $user->id . '" onclick="if(!confirm(\'' . htmlentities($i18n['confirmation'], ENT_QUOTES) . '\')) return false">' . $i18n['delete'] . '</a></td>
+		<td><a href="javascript:confirm_user(\'' . htmlentities($user->nombre, ENT_QUOTES, 'UTF-8') . '\',' . $user->id . '); return false">Facepalm!</a></td>
+		<td><a href="index.php?borrar=' . $user->id . '" onclick="if(!confirm(\'' . htmlentities($i18n['confirmation'], ENT_QUOTES, 'UTF-8') . '\')) return false">' . $i18n['delete'] . '</a></td>
 		<td width="60%"><div style="background:blue; width: '. round((time() - $user->fecha) / 60 / 60 / 24 * 100 / 30,2) . '%; height:10px;"</td>
 	</tr>';
 }
@@ -96,7 +102,7 @@ foreach ($users as $user)
 <?php
 echo '</table></form>';
 echo $fb_button;
-if ($uid > 0) echo ' <a href="' . $facebook->getLogoutUrl() .'" onclick="if (!confirm(\'' . htmlentities($i18n['facebook_logout_warning'], ENT_QUOTES) .'\')) return false;">' . htmlentities($i18n['facebook_logout']) .'</a>';
+if ($uid > 0) echo ' <a href="' . $facebook->getLogoutUrl() .'" onclick="if (!confirm(\'' . htmlentities($i18n['facebook_logout_warning'], ENT_QUOTES, 'UTF-8') .'\')) return false;">' . htmlentities($i18n['facebook_logout'], ENT_QUOTES, 'UTF-8') .'</a>';
 ?>
 
 <div>
@@ -124,3 +130,5 @@ computer, wrote what might be the first programming textbook in 1951,
 	finding errors in my own programs."</p>
 <img src="facepalm1.jpg" /><br /><br />
 <object width="480" height="385"><param name="movie" value="http://www.youtube.com/v/wjLgekyOZA0&hl=en_US&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/wjLgekyOZA0&hl=en_US&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed></object>
+</body>
+</html>
