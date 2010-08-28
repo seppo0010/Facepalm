61b0af2fb374ea93b4675c269a08aca2622996a2
Seppo <seppo0010@gmail.com>
2010-08-27 21:48:14 -0300

    i18n kick off

---
+++ web/i18n/es.php
@@ -0,0 +1,14 @@
+<?php
+$i18n['facebook_disconnect'] = 'Desconectar mi Cuenta de Facebook';
+$i18n['facebook_connect'] = 'Conectarse con Facebook';
+$i18n['facebook_logout'] = 'Salir de Facebook';
+$i18n['facebook_logout_warning'] = 'Esto no va a evitar que se posteen tus facepalms.';
+$i18n['reason_to_facepalm_nickname'] = 'Razón del Facepalm para ';
+$i18n['description'] = "Una aplicación que trackea cuánto tiempo pasamos sin pensar \"qué boludo que soy\" al son de una palmada en la frente.\nCada vez que uno se manda una boludez, debe venir a clickear \"facepalm\" para resetear el contador.";
+$i18n['delete'] = 'Borrar';
+$i18n['confirmation'] = 'tas seguro, flakito?';
+$i18n['save'] = 'Guardar';
+$i18n['welcome'] = 'Bienvenido';
+$i18n['name'] = 'Nombre';
+$i18n['date'] = 'Fecha';
+$i18n['you'] = 'Vos';
